c37c8124b6a5a3aff4ae59e983ab81fdcc25ce02
mstill3 <stillwell006@gmail.com>
2019-06-06 21:28:50 -0400

    walk and jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    // // Start is called before the first frame update
+    // void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        // controller.Move();   
+        // Debug.Log(horizontalMove);
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            Debug.Log("J");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //move character
+        // constant speed
+        // Debug.Log(horizontalMove);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
